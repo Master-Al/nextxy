ce0c3ddae89ce77e77138e57c0fdc139c5674e63
Al Pandis <al.pandis26@gmail.com>
2022-01-17 12:07:50 +0800

    Add input

--- pages/soon.tsx
+++ pages/soon.tsx
@@ -16,6 +16,28 @@ class Soon extends React.Component {
           <div className="w-full">
             <div className="Soon">ROLYN SOON</div>
           </div>
+          <div className="desc row w-full">
+            <div>
+              <div className="container h-screen flex justify-center items-center">
+                <div className="getQoute">
+                  Get A Quote while We’re Rolyn the Website
+                </div>
+                <div className="relative">
+                  <input
+                    type="text"
+                    className="h-14 w-96 pl-10 pr-20 rounded-lg z-0 focus:shadow focus:outline-none"
+                    placeholder="Input your Email Address"
+                  />
+                  <div className="absolute top-2 right-2">
+                    {" "}
+                    <button className="h-10 w-20 text-white rounded-lg text-white bg-[#266599]">
+                      Get a Qoute
+                    </button>{" "}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
